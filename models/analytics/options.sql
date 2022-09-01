fc53d4db9b345c9be3d6e990a5533b009e5786d5
Paul Johnson <100304042+PJ-Improving@users.noreply.github.com>
2022-09-01 14:59:43 -0500

    Update options.sql

--- models/analytics/options.sql
+++ models/analytics/options.sql
@@ -1,8 +1,5 @@
-{{
-  config(materialized='table'  ,
-  schema='ANALYTICS'
-)
-}}
+{{ config(alias='TYPES'
+         ) }}
 
 SELECT 
     c.clientid
@@ -11,6 +8,6 @@ SELECT
     , o.NAME
     , o.VALUE
     ,current_timestamp ROW_INSERT_TS
-FROM POC.HVMG_raw.OPTIONS o
+FROM POC.RAW.OPTIONS o
 join poc.analytics.clients c
 on o.client_nm =c.name
